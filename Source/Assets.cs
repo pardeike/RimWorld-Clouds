452dd084726fcde7561f3d62ce4472a41f2e27e1
Andreas Pardeike <andreas@pardeike.net>
2023-09-22 02:55:16 +0200

    initial checkin

---
+++ Source/Assets.cs
@@ -0,0 +1,84 @@
+﻿using HarmonyLib;
+using System.IO;
+using UnityEngine;
+using Verse;
+
+namespace Clouds
+{
+	[StaticConstructorOnStartup]
+	public static class Assets
+	{
+		static readonly AssetBundle assets = LoadAssetBundle();
+		static readonly GameObject cloudSystem = assets.LoadAsset<GameObject>("CloudSystem");
+		static GameObject clouds;
+		static ParticleSystem particles;
+		static float baseSpeed;
+
+		static Assets()
+		{
+			Object.DontDestroyOnLoad(cloudSystem);
+		}
+
+		public static GameObject CreateClouds()
+		{
+			clouds = Object.Instantiate(cloudSystem);
+			particles = clouds.GetComponent<ParticleSystem>();
+			baseSpeed = particles.main.simulationSpeed;
+			return clouds;
+		}
+
+		public static bool IsLoaded => clouds != null && particles != null;
+		public static float BaseSpeed => baseSpeed;
+
+		public static bool Active
+		{
+			get => clouds.activeSelf;
+			set => clouds.SetActive(value);
+		}
+
+		public static bool Pause
+		{
+			get => particles.isPaused;
+			set
+			{
+				if (value)
+					particles.Pause();
+				else
+					particles.Play();
+			}
+		}
+
+		public static float Speed
+		{
+			get => particles.main.simulationSpeed;
+			set
+			{
+				var main = particles.main;
+				main.simulationSpeed = value;
+			}
+		}
+
+		public static float Angle
+		{
+			get => clouds.transform.rotation.eulerAngles.y;
+			set
+			{
+				var eulerAngles = clouds.transform.rotation.eulerAngles;
+				eulerAngles.y = value;
+				clouds.transform.rotation = Quaternion.Euler(eulerAngles);
+			}
+		}
+
+		public static string GetModRootDirectory()
+		{
+			var me = LoadedModManager.GetMod<Clouds_Main>();
+			return me.Content.RootDir;
+		}
+
+		public static AssetBundle LoadAssetBundle()
+		{
+			var path = Path.Combine(GetModRootDirectory(), "Resources", "clouds");
+			return AssetBundle.LoadFromFile(path);
+		}
+	}
+}
---
+++ Source/Main.cs
@@ -0,0 +1,75 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace Clouds
+{
+	public class Clouds_Main : Mod
+	{
+		public Clouds_Main(ModContentPack content) : base(content)
+		{
+			var harmony = new Harmony("net.pardeike.clouds");
+			harmony.PatchAll();
+		}
+	}
+
+	[HarmonyPatch(typeof(Map), nameof(Map.FinalizeInit))]
+	public static class Map_FinalizeInit_Patch
+	{
+		public static void Postfix(Map __instance)
+		{
+			var clouds = Assets.CreateClouds();
+			Assets.Speed = 0.5f;
+
+			var alt = AltitudeLayer.MetaOverlays.AltitudeFor();
+			clouds.transform.position = new Vector3(__instance.Size.x / 2f, alt, __instance.Size.z / 2f);
+			var max = Math.Max(__instance.Size.x, __instance.Size.z);
+			clouds.transform.localScale = Vector3.one * max / 25f;
+		}
+	}
+
+	[HarmonyPatch(typeof(TickManager), nameof(TickManager.DoSingleTick))]
+	public static class TickManager_DoSingleTick_Patch
+	{
+		static float nextAngle = -90f;
+
+		public static void Postfix()
+		{
+			if (Rand.Chance(0.001f))
+				nextAngle = (nextAngle + Rand.Range(-10, 10f) + 360) % 360;
+
+			var currentAngle = Assets.Angle;
+
+			var delta = nextAngle - currentAngle;
+			var absDelta = Math.Abs(delta);
+			if (absDelta > 180)
+				delta = delta > 0 ? delta - 360 : delta + 360;
+			currentAngle += delta * 0.001f;
+			currentAngle = (currentAngle + 360) % 360;
+
+			Assets.Angle = currentAngle;
+		}
+	}
+
+	[HarmonyPatch]
+	public static class Current_ProgramState_Patch
+	{
+		public static IEnumerable<MethodBase> TargetMethods()
+		{
+			yield return AccessTools.Method(typeof(TickManager), nameof(TickManager.TogglePaused));
+			yield return AccessTools.PropertySetter(typeof(TickManager), nameof(TickManager.CurTimeSpeed));
+		}
+
+		public static void Postfix(TickManager __instance)
+		{
+			if (__instance != null && Current.Game != null)
+			{
+				Assets.Pause = __instance.Paused;
+				Assets.Speed = Assets.BaseSpeed * __instance.TickRateMultiplier;
+			}
+		}
+	}
+}
